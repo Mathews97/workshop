43be32df0eb04da34f5a0ec4f874afb485a813bd
Mathews97 <root@11CPU0221L.amritavidya.edu>
2016-06-07 13:01:50 +0530

    Creating chocolate store-3rd stage

--- registration/urls.py
+++ registration/urls.py
@@ -9,6 +9,8 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
 url(r'^$', UserRegistrationView.as_view(), name='register_user'),
 url(r'^user/success/', TemplateView.as_view(template_name='success.html'),
-name='page'),
+                            name='page'),
 url(r'^chocolate/add/',AddChocolateView.as_view(),name="add_chocolate"),
+url( r'^chocolate/info/(?P<choco_id>\d+)/$', ChocolateDetailsView.as_view(), name="chocolate_info"),
+url(r'^user/profile/edit/$', UserProfileUpdateView.as_view(), name='user_update_form'),
 ]
